0681d9f9dfa5ed6a2e21acba6959aa98571ccf06
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-28 19:21:25 +0530

    connecting to db

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import { database, password } from "pg/lib/defaults";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "learningAuth",
+  password: "Pointbreak",
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
